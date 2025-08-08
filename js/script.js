5d739a7366af3e3c475fe3841ba7d01bc59c5dc6
Artyom Soloveyov <soloviy.shpak16@gmail.com>
2025-08-08 12:33:18 +0300

    Добавлена прокрутка наверх в футоре

--- js/script.js
+++ js/script.js
@@ -1,2 +1,6 @@
-// JavaScript код 
-console.log('Привет, Artyom!'); 
+const footerScrollUp = $('.footer__btn');
+
+footerScrollUp.on('click', (e) => {
+  e.preventDefault();
+  $('html, body').animate({ scrollTop: 0 }, 'smooth');
+});
