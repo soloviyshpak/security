979bc1e3d9fc12ea6016f87d0ff3708d313b085b
Artyom Soloveyov <soloviy.shpak16@gmail.com>
2025-08-08 20:45:40 +0300

    Фикс прилипающей навигации

--- js/script.js
+++ js/script.js
@@ -1,25 +1,27 @@
-// При 50 пикселях надо приклеить меню
-
+// Переменные для удобства работы
 const header = $('.header');
 const headerWrapper = $('.header__wrapper');
 const footerScrollUp = $('.footer__btn');
 
+// Прилипающая навигация при скролле
 $(window).on('scroll', () => {
-  if (scrollY > 50) {
+  if (scrollY >= 50) {
     header.addClass('header--scrolled');
     headerWrapper.addClass('header__wrapper--scrolled');
   }
-  if (scrollY < 50) {
+  if (scrollY <= 50) {
     header.removeClass('header--scrolled');
     headerWrapper.removeClass('header__wrapper--scrolled');
   }
 });
 
+// Кнопка для проматывания на вверх в футоре
 footerScrollUp.on('click', (e) => {
   e.preventDefault();
   $('html, body').animate({ scrollTop: 0 }, 'smooth');
 });
 
+// Галерея
 Fancybox.bind('[data-fancybox]', {
   // Your custom options
 });
