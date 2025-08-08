2bad1f834715d75feb9ebbd68e4f8375fca7b028
Artyom Soloveyov <soloviy.shpak16@gmail.com>
2025-08-08 20:41:34 +0300

    Теперь Header прилипает к верху при скролле

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
+// При 50 пикселях надо приклеить меню
+
+const header = $('.header');
+const headerWrapper = $('.header__wrapper');
 const footerScrollUp = $('.footer__btn');
+
+$(window).on('scroll', () => {
+  if (scrollY > 50) {
+    header.addClass('header--scrolled');
+    headerWrapper.addClass('header__wrapper--scrolled');
+  }
+  if (scrollY < 50) {
+    header.removeClass('header--scrolled');
+    headerWrapper.removeClass('header__wrapper--scrolled');
+  }
+});
 
 footerScrollUp.on('click', (e) => {
   e.preventDefault();
